cffc800ca0c92a78761142989d7965ddfa84a410
Test User <test@example.com>
2026-01-17 02:14:49 +0600

    ONek kisu kore felsi tw

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.AppUser;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public AppUser create(@RequestBody AppUser user) {
+        return service.createUser(user);
+    }
+
+    @GetMapping
+    public List<AppUser> getAll() {
+        return service.getAllUsers();
+    }
+}
