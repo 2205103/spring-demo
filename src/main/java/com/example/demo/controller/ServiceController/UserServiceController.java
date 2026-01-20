da1a8d291827d190f10a496ec7b0edff02500f4d
Test User <test@example.com>
2026-01-21 03:02:08 +0600

    Sami Maruf Fuad 6

--- src/main/java/com/example/demo/controller/ServiceController/UserServiceController.java
+++ src/main/java/com/example/demo/controller/ServiceController/UserServiceController.java
@@ -51,6 +51,6 @@ public class UserServiceController {
         userService.createUser("Sami Sarwar", "103");
         userService.createUser("Maruf Hasan Rahim", "104");
         userService.createUser("Fuad Shahriar", "164");
-        return "Initial data createdddddddddddddddddd!";
+        return "Initial data createdddddddddddddddddddddddd!";
     }
 }
